eef771399a7d8603eca202e7b52663ff60485dc0
chenlianghong <clh021@gmail.com>
2021-09-24 14:05:36 +0800

    web: add eslint

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -2,21 +2,19 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   extends: [
     'plugin:vue/essential',
     'airbnb-base',
+    // 在执行 eslint--fix 命令时，ESLint 就会按照 Prettier 的配置规则来格式化代码，解决二者冲突问题
+    'plugin:prettier/recommended' // 添加 prettier 插件
   ],
   parserOptions: {
     ecmaVersion: 12,
     parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
-};
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {}
+}
