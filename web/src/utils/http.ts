6901c4f3cfe28e12bd9acad93fcabfa09627851f
chenlianghong <clh021@gmail.com>
2021-09-24 14:36:00 +0800

    web: fix typescript

--- web/src/utils/http.ts
+++ web/src/utils/http.ts
@@ -1,4 +1,3 @@
-
 import axios from 'axios'
 // import { Message } from 'element-plus'
 
@@ -29,7 +28,7 @@ instance.interceptors.request.use(
   }
 )
 
-const errorHandle = (status, other) => {
+const errorHandle = (status: number, other: string) => {
   switch (status) {
     case 400:
       Message.error('信息校验失败')
