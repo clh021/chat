b6a9693c5161b5c87b53c966b803a6a506dc5e44
chenlianghong <clh021@gmail.com>
2022-02-25 21:10:06 +0800

    rtc-test: message-box with data

--- web/src/components/frame/rtc-test/chat-box/message-box.vue
+++ web/src/components/frame/rtc-test/chat-box/message-box.vue
@@ -1,80 +1,25 @@
 <template>
   <div class="messages flex-1 overflow-auto">
-    <div class="message mb-4 flex">
-      <div class="flex-2">
+    <div
+      :class="`message mb-4 flex${msg.isSelf ? ' me text-right' : ''}`"
+      v-for="(msg, index) in messages"
+      :key="`msg_${index}`"
+    >
+      <div class="flex-2" v-if="!msg.isSelf">
         <div class="w-12 h-12 relative">
-          <img
-            class="w-12 h-12 rounded-full mx-auto"
-            src="/twc/resources/profile-image.png"
-            alt="chat-user"
-          />
+          <img class="w-12 h-12 rounded-full mx-auto" :src="msg.img" alt="chat-user" />
           <span
             class="absolute w-4 h-4 bg-gray-400 rounded-full right-0 bottom-0 border-2 border-white"
           ></span>
         </div>
       </div>
       <div class="flex-1 px-2">
-        <div class="inline-block bg-gray-300 rounded-full p-2 px-6 text-gray-700">
-          <span
-            >Hey there. We would like to invite you over to our office for a visit. How about
-            it?</span
-          >
-        </div>
-        <div class="pl-4"><small class="text-gray-500">15 April</small></div>
-      </div>
-    </div>
-    <div class="message mb-4 flex">
-      <div class="flex-2">
-        <div class="w-12 h-12 relative">
-          <img
-            class="w-12 h-12 rounded-full mx-auto"
-            src="/twc/resources/profile-image.png"
-            alt="chat-user"
-          />
-          <span
-            class="absolute w-4 h-4 bg-gray-400 rounded-full right-0 bottom-0 border-2 border-white"
-          ></span>
-        </div>
-      </div>
-      <div class="flex-1 px-2">
-        <div class="inline-block bg-gray-300 rounded-full p-2 px-6 text-gray-700">
-          <span>All travel expenses are covered by us of course :D</span>
-        </div>
-        <div class="pl-4"><small class="text-gray-500">15 April</small></div>
-      </div>
-    </div>
-    <div class="message me mb-4 flex text-right">
-      <div class="flex-1 px-2">
-        <div class="inline-block bg-blue-600 rounded-full p-2 px-6 text-white">
-          <span>It's like a dream come true</span>
-        </div>
-        <div class="pr-4"><small class="text-gray-500">15 April</small></div>
-      </div>
-    </div>
-    <div class="message me mb-4 flex text-right">
-      <div class="flex-1 px-2">
-        <div class="inline-block bg-blue-600 rounded-full p-2 px-6 text-white">
-          <span>I accept. Thank you very much.</span>
-        </div>
-        <div class="pr-4"><small class="text-gray-500">15 April</small></div>
-      </div>
-    </div>
-    <div class="message mb-4 flex">
-      <div class="flex-2">
-        <div class="w-12 h-12 relative">
-          <img
-            class="w-12 h-12 rounded-full mx-auto"
-            src="/twc/resources/profile-image.png"
-            alt="chat-user"
-          />
-          <span
-            class="absolute w-4 h-4 bg-gray-400 rounded-full right-0 bottom-0 border-2 border-white"
-          ></span>
-        </div>
-      </div>
-      <div class="flex-1 px-2">
-        <div class="inline-block bg-gray-300 rounded-full p-2 px-6 text-gray-700">
-          <span>You are welome. We will stay in touch.</span>
+        <div
+          :class="`inline-block rounded-full p-2 px-6 ${
+            msg.isSelf ? 'bg-blue-600 text-white' : 'bg-gray-300 text-gray-700'
+          }`"
+        >
+          <span>{{ msg.content }}</span>
         </div>
         <div class="pl-4"><small class="text-gray-500">15 April</small></div>
       </div>
@@ -83,5 +28,24 @@
 </template>
 
 <script lang="ts">
-export default {}
+export default {
+  data() {
+    return {
+      messages: [
+        {
+          isSelf: false,
+          img: '/twc/resources/profile-image.png',
+          content: '可以邀请我的家人们都来试试吗？'
+        },
+        { isSelf: true, content: '好呀！' },
+        {
+          isSelf: false,
+          img: '/twc/resources/profile-image.png',
+          content: '啊哈！太谢谢你啦！'
+        },
+        { isSelf: true, content: '不客气哈！:D' }
+      ]
+    }
+  }
+}
 </script>
