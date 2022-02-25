cad0f704b236fe7ceba254697f24b301af6fe7b7
chenlianghong <clh021@gmail.com>
2022-02-25 20:19:10 +0800

    rtc-test: chat split component

---
+++ web/src/components/frame/rtc-test/chat-list.vue
@@ -0,0 +1,281 @@
+<template>
+  <div class="sidebar hidden lg:flex w-1/3 flex-2 flex-col pr-6">
+    <div class="search flex-2 pb-6 px-2">
+      <input
+        type="text"
+        class="outline-none py-2 block w-full bg-transparent border-b-2 border-gray-200"
+        placeholder="Search"
+      />
+    </div>
+    <div class="flex-1 h-full overflow-auto px-2">
+      <div
+        class="
+          entry
+          cursor-pointer
+          transform
+          hover:scale-105
+          duration-300
+          transition-transform
+          bg-white
+          mb-4
+          rounded
+          p-4
+          flex
+          shadow-md
+        "
+      >
+        <div class="flex-2">
+          <div class="w-12 h-12 relative">
+            <img
+              class="w-12 h-12 rounded-full mx-auto"
+              src="/twc/resources/profile-image.png"
+              alt="chat-user"
+            />
+            <span
+              class="
+                absolute
+                w-4
+                h-4
+                bg-green-400
+                rounded-full
+                right-0
+                bottom-0
+                border-2 border-white
+              "
+            ></span>
+          </div>
+        </div>
+        <div class="flex-1 px-2">
+          <div class="truncate w-32"><span class="text-gray-800">Ryann Remo</span></div>
+          <div><small class="text-gray-600">Yea, Sure!</small></div>
+        </div>
+        <div class="flex-2 text-right">
+          <div><small class="text-gray-500">15 April</small></div>
+          <div>
+            <small
+              class="
+                text-xs
+                bg-red-500
+                text-white
+                rounded-full
+                h-6
+                w-6
+                leading-6
+                text-center
+                inline-block
+              "
+            >
+              23
+            </small>
+          </div>
+        </div>
+      </div>
+      <div
+        class="
+          entry
+          cursor-pointer
+          transform
+          hover:scale-105
+          duration-300
+          transition-transform
+          bg-white
+          mb-4
+          rounded
+          p-4
+          flex
+          shadow-md
+        "
+      >
+        <div class="flex-2">
+          <div class="w-12 h-12 relative">
+            <img
+              class="w-12 h-12 rounded-full mx-auto"
+              src="/twc/resources/profile-image.png"
+              alt="chat-user"
+            />
+            <span
+              class="
+                absolute
+                w-4
+                h-4
+                bg-gray-400
+                rounded-full
+                right-0
+                bottom-0
+                border-2 border-white
+              "
+            ></span>
+          </div>
+        </div>
+        <div class="flex-1 px-2">
+          <div class="truncate w-32">
+            <span class="text-gray-800">Karp Bonolo</span>
+          </div>
+          <div><small class="text-gray-600">Yea, Sure!</small></div>
+        </div>
+        <div class="flex-2 text-right">
+          <div><small class="text-gray-500">15 April</small></div>
+          <div>
+            <small
+              class="
+                text-xs
+                bg-red-500
+                text-white
+                rounded-full
+                h-6
+                w-6
+                leading-6
+                text-center
+                inline-block
+              "
+            >
+              10
+            </small>
+          </div>
+        </div>
+      </div>
+      <div
+        class="
+          entry
+          cursor-pointer
+          transform
+          hover:scale-105
+          duration-300
+          transition-transform
+          bg-white
+          mb-4
+          rounded
+          p-4
+          flex
+          shadow-md
+          border-l-4 border-red-500
+        "
+      >
+        <div class="flex-2">
+          <div class="w-12 h-12 relative">
+            <img
+              class="w-12 h-12 rounded-full mx-auto"
+              src="/twc/resources/profile-image.png"
+              alt="chat-user"
+            />
+            <span
+              class="
+                absolute
+                w-4
+                h-4
+                bg-gray-400
+                rounded-full
+                right-0
+                bottom-0
+                border-2 border-white
+              "
+            ></span>
+          </div>
+        </div>
+        <div class="flex-1 px-2">
+          <div class="truncate w-32">
+            <span class="text-gray-800">Mercedes Yemelyan</span>
+          </div>
+          <div><small class="text-gray-600">Yea, Sure!</small></div>
+        </div>
+        <div class="flex-2 text-right">
+          <div><small class="text-gray-500">15 April</small></div>
+        </div>
+      </div>
+      <div
+        class="
+          entry
+          cursor-pointer
+          transform
+          hover:scale-105
+          duration-300
+          transition-transform
+          bg-white
+          mb-4
+          rounded
+          p-4
+          flex
+          shadow-md
+        "
+      >
+        <div class="flex-2">
+          <div class="w-12 h-12 relative">
+            <img
+              class="w-12 h-12 rounded-full mx-auto"
+              src="/twc/resources/profile-image.png"
+              alt="chat-user"
+            />
+            <span
+              class="
+                absolute
+                w-4
+                h-4
+                bg-gray-400
+                rounded-full
+                right-0
+                bottom-0
+                border-2 border-white
+              "
+            ></span>
+          </div>
+        </div>
+        <div class="flex-1 px-2">
+          <div class="truncate w-32">
+            <span class="text-gray-800">Cadi Kajetán</span>
+          </div>
+          <div><small class="text-gray-600">Yea, Sure!</small></div>
+        </div>
+        <div class="flex-2 text-right">
+          <div><small class="text-gray-500">15 April</small></div>
+        </div>
+      </div>
+      <div
+        class="
+          entry
+          cursor-pointer
+          transform
+          hover:scale-105
+          duration-300
+          transition-transform
+          bg-white
+          mb-4
+          rounded
+          p-4
+          flex
+          shadow-md
+        "
+      >
+        <div class="flex-2">
+          <div class="w-12 h-12 relative">
+            <img
+              class="w-12 h-12 rounded-full mx-auto"
+              src="/twc/resources/profile-image.png"
+              alt="chat-user"
+            />
+            <span
+              class="
+                absolute
+                w-4
+                h-4
+                bg-gray-400
+                rounded-full
+                right-0
+                bottom-0
+                border-2 border-white
+              "
+            ></span>
+          </div>
+        </div>
+        <div class="flex-1 px-2">
+          <div class="truncate w-32">
+            <span class="text-gray-800">Rina Samuel</span>
+          </div>
+          <div><small class="text-gray-600">Yea, Sure!</small></div>
+        </div>
+        <div class="flex-2 text-right">
+          <div><small class="text-gray-500">15 April</small></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
