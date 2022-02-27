113724f4f06712b0f7fb4b9f04ee97a31af1a65d
chenlianghong <clh021@gmail.com>
2022-02-27 11:45:18 +0800

    rtc-test: input-box update btns

--- web/src/components/frame/rtc-test/chat-box/input-box.vue
+++ web/src/components/frame/rtc-test/chat-box/input-box.vue
@@ -47,7 +47,52 @@
         </span>
       </div> -->
       <div class="flex-1">
-        <button class="bg-blue-400 w-10 h-10 rounded-full inline-block">
+        <label class="swap swap-rotate">
+          <!-- this hidden checkbox controls the state -->
+          <input type="checkbox" />
+
+          <!-- 发送 icon -->
+          <svg
+            class="swap-on fill-current w-10 h-10"
+            version="1.0"
+            xmlns="http://www.w3.org/2000/svg"
+            width="50"
+            height="50"
+            viewBox="0 0 50 50"
+            preserveAspectRatio="xMidYMid meet"
+          >
+            <g transform="translate(0,50) scale(0.100000,-0.100000)" fill="#000000" stroke="none">
+              <path
+                d="M155 456 c-60 -28 -87 -56 -114 -116 -36 -79 -19 -183 42 -249 33
+-36 115 -71 167 -71 52 0 134 35 167 71 34 37 63 110 63 159 0 52 -35 134 -71
+167 -37 34 -110 63 -159 63 -27 0 -65 -10 -95 -24z m180 -15 c128 -58 164
+-223 72 -328 -101 -115 -283 -88 -348 52 -79 171 104 354 276 276z"
+              />
+              <path
+                d="M190 335 c-29 -30 -51 -58 -47 -62 4 -4 28 13 52 37 l45 44 0 -122
+c0 -75 4 -122 10 -122 6 0 10 47 10 122 l0 122 45 -44 c24 -24 48 -41 52 -37
+6 7 -94 117 -107 117 -3 0 -30 -25 -60 -55z"
+              />
+            </g>
+          </svg>
+
+          <!-- 加号 icon -->
+          <svg
+            class="swap-off fill-current w-10 h-10"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="50"
+            height="50"
+            viewBox="0 0 50 50"
+            style="fill: #000000"
+          >
+            <path
+              d="M 25 2 C 12.309295 2 2 12.309295 2 25 C 2 37.690705 12.309295 48 25 48 C 37.690705 48 48 37.690705 48 25 C 48 12.309295 37.690705 2 25 2 z M 25 4 C 36.609824 4 46 13.390176 46 25 C 46 36.609824 36.609824 46 25 46 C 13.390176 46 4 36.609824 4 25 C 4 13.390176 13.390176 4 25 4 z M 24 13 L 24 24 L 13 24 L 13 26 L 24 26 L 24 37 L 26 37 L 26 26 L 37 26 L 37 24 L 26 24 L 26 13 L 24 13 z"
+            ></path>
+          </svg>
+        </label>
+        <!-- <button class="bg-blue-400 w-10 h-10 rounded-full inline-block">
           <span class="inline-block align-text-bottom">
             <svg
               fill="none"
@@ -61,7 +106,7 @@
               <path d="M5 13l4 4L19 7"></path>
             </svg>
           </span>
-        </button>
+        </button> -->
       </div>
     </div>
   </div>
