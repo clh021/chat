8e5dd1a6e046ba485c86ca5673f9a7f3a380adc9
chenlianghong <clh021@gmail.com>
2022-03-04 16:48:19 +0800

    web: add utils plugin

--- web/src/main.js
+++ web/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import Root from './App.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+const app = createApp(Root)
+
+import utils from './utils'
+app.use(utils);
+
+app.mount('#app')
