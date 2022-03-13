ee2eb55ed3e561fcfbbf5855ac53dea24af4ab2b
chenlianghong <clh021@gmail.com>
2022-03-13 22:31:53 +0800

    web: add next TODO3

--- web/src/components/frame/rtc-test/chat-box/index.vue
+++ web/src/components/frame/rtc-test/chat-box/index.vue
@@ -45,6 +45,7 @@ export default {
     // TODO: 建立连接的过程可以搞一个系统日志打开
     // TODO: 所有建立连接的过程都放到系统日志里面去
     // TODO: 日志中需要较为方便的看出是否是 p2p 连接
+    // TODO: 方便转发或者别的方式记录收藏内容
     console.log('mounted.')
     this.rtc = new rtc('test')
     this.rtc.handles(this.openHandle, this.connHandle)
