59c154307025b15efa4560c6d415cf9f2dcba376
chenlianghong <clh021@gmail.com>
2022-03-25 22:47:15 +0800

    web: msgBox support system msg

--- web/src/components/frame/rtc-test/chat-box/message/sys.vue
+++ web/src/components/frame/rtc-test/chat-box/message/sys.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="message mb-4 flex me text-right">
+  <div class="message mb-4 flex sys text-center">
     <div class="flex-1 px-2">
-      <div class="inline-block rounded-full p-2 px-6 bg-gray-300 text-gray-700">
+      <div class="text-sm text-gray-400">
         <span>{{ msg.content }}</span>
       </div>
-      <div v-if="msg.time" class="pl-4">
+      <!--div v-if="msg.time" class="pl-4">
         <small class="text-gray-500">{{ msg.time }}</small>
-      </div>
+      </div-->
     </div>
   </div>
 </template>
